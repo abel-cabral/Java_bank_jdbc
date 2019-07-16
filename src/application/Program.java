eb7ee6171256b5b77af1b8ac7edc9dce405d4641
warakia <jcabral@id.uff.br>
2019-07-15 23:27:40 -0300

    Seller entiity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Department department = new Department(1, "Books");
+		Seller seller = new Seller(1, "Renia Casé", "regininhag@gmail.com", new Date(), 1250.00, department);
+		System.out.println(seller);
 
 	}
 
