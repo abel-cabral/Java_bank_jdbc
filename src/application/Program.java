b750a92388e039860cfa1548a0c2f1928ff45874
warakia <jcabral@id.uff.br>
2019-07-18 23:55:55 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,14 @@
 package application;
 
-import java.util.Date;
-
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
-		Department department = new Department(1, "Books");
-		Seller seller = new Seller(1, "Renia Casé", "regininhag@gmail.com", new Date(), 1250.00, department);
+	public static void main(String[] args) {		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(4);
 		System.out.println(seller);
 
 	}
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,34 +1,69 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
+	private Connection conn;
+
+	public SellerDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
 
 	@Override
 	public void insert(Seller obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void update(Seller obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public Seller findById(Integer client) {
-		// TODO Auto-generated method stub
-		return null;
+	public Seller findById(Integer id) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName\n" + "FROM seller INNER JOIN department\n"
+							+ "ON seller.DepartmentId = department.Id\n" + "WHERE seller.Id = ?");
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			// rs inicia no 0 da tabela, precisamos antes de avançar testar se nao é null
+			if (rs.next()) {
+				Department dep = new Department(rs.getInt("DepartmentId"), rs.getString("DepName"));
+				Seller sel = new Seller(rs.getInt("Id"), rs.getString("Name"), rs.getString("Email"),
+						rs.getDate("BirthDate"), rs.getDouble("BaseSalary"), dep);
+				return sel;
+			}
+			return null;
+
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+
 	}
 
 	@Override
